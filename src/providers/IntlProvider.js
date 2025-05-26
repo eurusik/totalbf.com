3fcbfe73c8101e4dd8b88a9602de5dc7619168d4
Eugene Rusakov <john.rusakov@gmail.com>
2025-05-26 16:04:32 +0300

    Configures internationalization settings
    Updates internationalization configuration to use specific locales and enables locale detection.
    
    Sets the default timezone for date and time formatting.

--- src/providers/IntlProvider.js
+++ src/providers/IntlProvider.js
@@ -1,10 +1,8 @@
-'use client';
-
 import { NextIntlClientProvider } from 'next-intl';
 
 export function IntlProvider({ locale, messages, children }) {
   return (
-    <NextIntlClientProvider locale={locale} messages={messages}>
+    <NextIntlClientProvider locale={locale} messages={messages} timeZone="Europe/Kiev" now={new Date()}>
       {children}
     </NextIntlClientProvider>
   );
