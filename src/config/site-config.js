f3b0327eebb894ec8f2d54cbb6a2a757c028ed0d
Eugene Rusakov <john.rusakov@gmail.com>
2025-05-30 09:03:06 +0300

    added google analytics
    added Google Analytics with configurable settings
    
    added google analytics next.js way
    
    fix
    
    fix: implement Google Analytics using next/script
    
    fix: implement Google Analytics using next/script

--- src/config/site-config.js
+++ src/config/site-config.js
@@ -1,6 +1,10 @@
 const siteConfig = {
   siteUrl: 'https://totalbf.com',
   defaultLocale: 'uk',
+  analytics: {
+    googleAnalyticsId: 'G-LXGXRMJHCC',
+    enabled: true,
+  },
 };
 
 export default siteConfig;
