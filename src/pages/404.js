998a46270458fa1bcc2c98eb2cf72019b70e395c
Eugene Rusakov <john.rusakov@gmail.com>
2025-06-30 18:37:16 +0300

    fix seo 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,28 +1,36 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import { Helmet } from 'react-helmet';
 
 import Layout from 'components/Layout';
 import Section from 'components/Section';
 import Container from 'components/Container';
 import usePageMetadata from 'hooks/use-page-metadata';
 import { getSiteMetadata } from 'lib/site';
+import { helmetSettingsFromMetadata } from 'lib/site';
 
 import styles from 'styles/pages/Error.module.scss';
 
 export default function Custom404({ customPageTitle }) {
-  usePageMetadata({
+  const { metadata } = usePageMetadata({
     metadata: {
       title: customPageTitle,
       description: 'Сторінку не знайдено',
     },
   });
 
+  const helmetSettings = helmetSettingsFromMetadata(metadata);
+  const pageTitle = helmetSettings.title || customPageTitle;
+  const helmetSettingsWithoutTitle = { ...helmetSettings };
+  delete helmetSettingsWithoutTitle.title;
+
   return (
     <Layout>
       <Head>
-        <title>{customPageTitle}</title>
+        <title>{pageTitle}</title>
         <meta name="robots" content="noindex, nofollow" />
       </Head>
+      <Helmet {...helmetSettingsWithoutTitle} />
       <Section>
         <Container>
           <div className={styles.errorContainer}>
